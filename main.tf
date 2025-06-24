3ca6d43310236c06e6f4d315e8adb77560b46e4d
Vessie Bakalov <vesselinabakalov@gmail.com>
2025-06-24 13:22:05 -0400

    Create new dev vpc

--- main.tf
+++ main.tf
@@ -18,11 +18,31 @@ data "aws_vpc" "default" {
   default = true  
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -35,7 +55,7 @@ module "blog_sg" {
   version = "5.3.0"
   name = "blog_new"
 
-  vpc_id              = data.aws_vpc.default.id
+  vpc_id              = module.blog_vpc.id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
@@ -44,39 +64,3 @@ module "blog_sg" {
 }
 
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out."
-
-  vpc_id      = data.aws_vpc.default.id
-}
-
-resource  "aws_security_group_rule" "blog_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource  "aws_security_group_rule" "blog_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource  "aws_security_group_rule" "blog_everything_out" {
-  type = "egress"
-  from_port = 80
-  to_port = 80
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
